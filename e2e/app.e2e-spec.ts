c65208134b007467ddf2d10e28f7b2d11d9f523e
Norbert Virth <norbert.virth@gmail.com>
2017-10-01 15:51:53 +0200

    Updated the porject with updated angular cli: ng new
    Previous CLI version: 1.1.3
    Actual   CLI version: 1.4.4

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { BookteraAngularPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('booktera-angular App', () => {
-  let page: BookteraAngularPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new BookteraAngularPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
