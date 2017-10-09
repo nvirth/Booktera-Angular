6ae5c98da49b6d57ef75bab7ff0e08481ab06bca
Norbert Virth <norbert.virth@gmail.com>
2017-10-09 19:02:03 +0200

    Generated: Component: no-content
    ng g c no-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {HttpClientModule} from "@angular/common/http";
 import {CategoryManagerService} from "./manager-services/category-manager.service";
 import {CategorySelectorComponent} from './category-selector/category-selector.component';
 import {TreeNodeComponent} from './category-selector/tree-node/tree-node.component';
+import {NoContentComponent} from './no-content/no-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CategorySelectorComponent,
-    TreeNodeComponent
+    TreeNodeComponent,
+    NoContentComponent
   ],
   imports: [
     BrowserModule,
